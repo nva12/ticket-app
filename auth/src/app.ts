1d5aa31207b8d9bb8843c1ed6871e17f7378e333
nva12 <nvallee.a7@gmail.com>
2020-11-11 13:52:14 +0400

    implement automated testing for auth service

---
+++ auth/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true); // because we go through ingress-nginx proxy
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // no encryption to improve cross-language compatibility
+    secure: process.env.NODE_ENV !== 'test', // set cookie with http in test mode, only with https otherwise
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
