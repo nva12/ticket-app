192c999f4d85885c28dd97eb0ce00b0c449c5bea
nva12 <nvallee.a7@gmail.com>
2020-11-13 10:35:14 +0400

    move auth error handling and middlewares to common npm package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ticketappnvdev/common';
 
 const router = express.Router();
 
