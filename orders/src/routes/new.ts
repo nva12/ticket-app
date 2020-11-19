b9f3e3f0a2aff062d76b7e022956a0183ca53527
nva12 <nvallee.a7@gmail.com>
2020-11-19 10:03:28 +0400

    add event publishers for orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -9,6 +9,8 @@ import {
 } from '@ticketappnvdev/common';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -49,6 +51,17 @@ router.post(
     await order.save();
 
     // publish an event saying an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      // status: order.status,
+      status: OrderStatus.Created,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
